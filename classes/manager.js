8af10ed6ea2f14dcf69a37a0890fe612791c15e4
David Sanchez <davidmsanchez250@gmail.com>
2021-02-22 20:10:06 -0500

    edited manager class

--- classes/manager.js
+++ classes/manager.js
@@ -1,5 +1,12 @@
 const Employee = require('./employee')
 
 class Manager extends Employee {
+    constructor(officeNumber){
+       // const name = grabbed from data from inquirer?
+       // const id = 
+       // const email = 
+        super(name,id,email);
+        this.officeNumber = officeNumber;
+    }
     
 }
